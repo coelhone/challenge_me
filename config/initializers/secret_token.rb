55080de38c13b0f6cfeec20c236f5ccbda9439b6
Bruno Coelho <bcoelho@MacBook-Pro-de-Bruno.local>
2013-10-22 23:14:05 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChallengeMe::Application.config.secret_key_base = 'd544e0d6b8e654adf1f96767605261841b2876b7a09cc318e8a64482105a5c7c39925a3105db7d02091d362ebbd3e22fe900f4c41f84ddcb0716686387204c7a'
