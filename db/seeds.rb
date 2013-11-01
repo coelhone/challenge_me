434f96d2b872fe68d8f898f13dbaab54e38ecead
Bruno Coelho <brunoflcoelho@gmail.com>
2013-11-01 21:55:09 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+#puts 'DEFAULT USERS'
+#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+#puts 'user: ' << user.name
+
+user = User.where(email: "test@test.pt").first_or_create(name: "test", password: "testtest")
+user2 = User.where(email: "test2@test2.pt").first_or_create(name: "test2", password: "test2test2")
+
+Challenge.where(user_id: user.id, name: "challenge_1", desc: "fazer uma soma").first_or_create
+Challenge.where(user_id: user.id, name: "challenge_2", desc: "fazer uma subtracção").first_or_create
+Challenge.where(user_id: user2.id, name: "challenge_3", desc: "fazer uma divisão").first_or_create
+Challenge.where(user_id: user2.id, name: "challenge_4", desc: "fazer uma mutliplicação balalalalalalalalalalalalaal").first_or_create
